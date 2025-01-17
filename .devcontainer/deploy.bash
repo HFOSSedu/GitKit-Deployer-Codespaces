a73091f28ff5f08d8051f5b8345fe978d4c225cd
Stoney Jackson <dr.stoney@gmail.com>
2025-01-17 17:53:47 +0000

    fix: path to python

--- .devcontainer/deploy.bash
+++ .devcontainer/deploy.bash
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-/home/root/kitscript/.venv/bin/python -m kitscript "${SCRIPT_DIR}/deploy.yaml"
+/home/root/.local/kitscript/.venv/bin/python -m kitscript "${SCRIPT_DIR}/deploy.yaml"
