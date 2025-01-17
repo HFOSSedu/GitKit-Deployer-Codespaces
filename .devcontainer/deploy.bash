9d3375f0f52ff8443b8bd9839e334db9decaa6e6
Stoney Jackson <dr.stoney@gmail.com>
2025-01-17 17:47:50 +0000

    feat: deploy

---
+++ .devcontainer/deploy.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+/home/root/kitscript/.venv/bin/python -m kitscript "${SCRIPT_DIR}/deploy.yaml"
